160cb8cc98ee146bb08f51409eb7162b82c56e45
Josh Ford <thisisjoshford@gmail.com>
2020-01-22 18:42:08 -0800

    cart.js data hard coded

---
+++ data/cart.js
@@ -0,0 +1,32 @@
+const cartItem1 = {
+    name: 'Lyon\'s Collab',
+    amount: 1,
+    price: 1200.00,
+    
+};
+const cartItem2 = {
+    name: 'Curvy Erlflask',
+    amount: 2,
+    price: 420.00,
+    
+};
+const cartItem3 = {
+    name: 'Circuit Thumbprint',
+    amount: 4,
+    price: 150.00,
+    
+};
+const cartItem4 = {
+    name: 'Puffco',
+    amount: 3,
+    price: 1000.00,
+    
+};
+
+const glassOrder = [
+    cartItem1,
+    cartItem2,
+    cartItem3,
+    cartItem4
+];
+export default glassOrder;
