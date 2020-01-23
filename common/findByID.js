62f6e2c66dd5ecfb21cde89debb6295eedcf9bf6
Josh Ford <thisisjoshford@gmail.com>
2020-01-23 10:29:25 -0800

    TDD functionByID passed

---
+++ common/findByID.js
@@ -0,0 +1,8 @@
+const findByID = (glassID, glassArray) => {
+    for (let i = 0 ; i < glassArray.length; i++) {
+        const glassProduct = glassArray[i];
+        if (glassProduct.id === glassID){
+            return glassProduct;
+        }
+    } };
+export default findByID;
--- test/utils.js
+++ test/utils.js
@@ -1,11 +1,27 @@
+import findByID from '../common/findByID.js';
+import glassArt from '../data/glassArt.js';
 const test = QUnit.test;
-import findByID from 
-test('should return name when presented ID', function(assert) {
 
-    const glassID = '1' ;
-    const description = glassArt[1]; 
+test('should return Name/ Description when presented ID', function(assert) {
+    const glassArray = glassArt;
+    const glassID = 1 ;
     const expected = 'Lyon\'s Collab';
-    const result = findByID(glassID, description);
+    const result = findByID(glassID, glassArray);
+    assert.equal(result.name, expected);
+});
 
-    assert.equal(result, expected);
+test('should match Price when presented ID', function(assert) {
+    const glassArray = glassArt;
+    const glassID = 7 ;
+    const expected = 600;
+    const result = findByID(glassID, glassArray);
+    assert.equal(result.price, expected);
+});
+
+test('should match image when presented ID', function(assert) {
+    const glassArray = glassArt;
+    const glassID = 8 ;
+    const expected = '../assets/mini.png';
+    const result = findByID(glassID, glassArray);
+    assert.equal(result.image, expected);
 });
