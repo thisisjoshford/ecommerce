13d0e11a91a3a6f046a6f9b5952e509591306554
Josh Ford <thisisjoshford@gmail.com>
2020-01-24 08:47:42 -0800

    shopping cart auto populate

--- shopping-cart/renderLineItem.js
+++ shopping-cart/renderLineItem.js
@@ -1,19 +1,26 @@
+import calcLineItem from '../common/calcLineItem.js';
+
 function renderLineItem(lineItem, glassArt) {
     const glassArtTr = document.createElement('tr');
-    console.log(lineItem, glassArt);
     
-    const productName = document.createElement('td');
-    productName.textContent = glassArt.Name;
-    glassArtTr.appendChild(productName);
-    // console.log(glassOrderTr.Name);
-
     const productQuantity = document.createElement('td');
-    productQuantity.textContent = lineItem.amount;
+    productQuantity.textContent = lineItem.quantity;
     glassArtTr.appendChild(productQuantity);
     // console.log(glassOrderTr);
     
+    const productName = document.createElement('td');
+    productName.textContent = glassArt.name;
+    glassArtTr.appendChild(productName);
+    // console.log(glassOrderTr.Name);
+
     const productPrice = document.createElement('td');
     productPrice.textContent = glassArt.price;
+    glassArtTr.appendChild(productPrice);
+
+    const totalOrder = document.createElement('td');
+    const total = calcLineItem(lineItem.quantity, glassArt.price);
+    totalOrder.textContent = total;
+    glassArtTr.appendChild(totalOrder);
 
     // console.log(glassOrderTr);
 
